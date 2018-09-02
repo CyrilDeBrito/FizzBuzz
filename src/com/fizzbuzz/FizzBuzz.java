02f79b3a8e18d2c4dc07fbc47a6ce737137458df
CyrilDeBritoWindows <developpeur@cyrildebrito.info>
2018-09-02 20:17:10 +0200

    finish

--- src/com/fizzbuzz/FizzBuzz.java
+++ src/com/fizzbuzz/FizzBuzz.java
@@ -2,10 +2,22 @@ package com.fizzbuzz;
 
  class FizzBuzz {
 
+    String generate(int minNumber, int maxNumber) {
 
-    String generate(int number) {
-        if (number == 1)
-        return "1";
-        return "2";
+        String result = "";
+        while (minNumber <= maxNumber)
+            result += evaluateNumber(minNumber++);
+        return result;
     }
-}
+
+        private String evaluateNumber (int number){
+                if (number % 15 == 0)
+                    return "FizzBuzz";
+                if (number % 3 == 0)
+                    return "Fizz";
+                else if (number % 5 == 0)
+                    return "Buzz";
+                return String.valueOf(number);
+        }
+ }
+
--- src/com/fizzbuzz/FizzBuzzTest.java
+++ src/com/fizzbuzz/FizzBuzzTest.java
@@ -1,18 +1,79 @@
 package com.fizzbuzz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FizzBuzzTest {
 
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    public void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    // Test only they Bundary(fr : extreme) case
+
     @Test
     public void shouldReturnOneIfNumberIsOne() {
-        assertEquals("1", new FizzBuzz().generate(1));
+        assertEquals("1", fizzBuzz.generate(1, 1));
     }
 
     @Test
-    public void shouldReturnTwoIfNumberIsTwo(){
-        assertEquals("2", new FizzBuzz().generate(2));
+    public void shouldReturnTwoIfNumberIsTwo() {
+        assertEquals("2", fizzBuzz.generate(2, 2));
+    }
+
+    @Test
+    public void shouldReturnFizzIfNumberIsTree() {
+        assertEquals("Fizz", fizzBuzz.generate(3, 3));
+    }
+
+    @Test
+    public void shouldReturnFizzIfNumberIsSix() {
+        assertEquals("Fizz", fizzBuzz.generate(6, 6));
+    }
+
+    @Test
+    public void shouldReturnBuzzIfNumberIsFive() {
+        assertEquals("Buzz", fizzBuzz.generate(5, 5));
+    }
+
+    @Test
+    public void shouldReturnBuzzIfNumberIsTen() {
+        assertEquals("Buzz", fizzBuzz.generate(10, 10));
+    }
+
+    @Test
+    public void shouldReturnFizzBuzzIfNumberIsFifteen() {
+        assertEquals("FizzBuzz", fizzBuzz.generate(15, 15));
+    }
+
+    @Test
+    public void shouldReturnFizzBuzzIfNumberIsThirty() {
+        assertEquals("FizzBuzz", fizzBuzz.generate(30, 30));
+    }
+
+    // 2 parameters here
+    @Test
+    public void shouldReturnOneTwoIfNumberAreOneandTwo() {
+        assertEquals("12", fizzBuzz.generate(1, 2));
+    }
+
+    @Test
+    public void shouldReturnOneTwoFizzIfNumberAreOneTwoThree() {
+        assertEquals("12Fizz", fizzBuzz.generate(1, 3));
+    }
+
+    @Test
+    public void shouldReturnOneTwoFizzFourBuzzIfNumberAreOneToFive() {
+        assertEquals("12Fizz4Buzz", fizzBuzz.generate(1, 5));
+    }
+
+    @Test
+    public void ShouldReturnOneToFifteenIfNumerAreOneToFifteen() {
+        assertEquals("12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz", fizzBuzz.generate(1, 15));
     }
 }
